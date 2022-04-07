1613e3f4f0eaef9c37a3adb80a6ad9343e22eefd
KwangWoongLee <lgu4821@gmail.com>
2022-04-08 08:24:01 +0900

    first commit

---
+++ ServerCore/Listener.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
+{
+    class Listener
+    {
+        Socket _listenSocket;
+        Action<Socket> _onAcceptHandler;
+
+        public void Init(IPEndPoint endPoint, Action<Socket> onAcceptHandler)
+        {
+            _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _onAcceptHandler += onAcceptHandler;
+
+            _listenSocket.Bind(endPoint);
+
+            _listenSocket.Listen(10);
+
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            // 해당 args에 콜백함수 등록
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            //최초 Accept Async 호출
+            RegisterAccept(args);
+        }
+
+        void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            bool pending = _listenSocket.AcceptAsync(args);
+
+            // pending == false 이면, Accept가 바로 수행됐음을 의미함
+            if (!pending)
+                OnAcceptCompleted(null , args);      
+        }
+
+        void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if (args.SocketError == SocketError.Success)
+            {
+                // Accept 됨 => 초기화 시에 등록한 EventHandler를 통해 Socket 리턴
+                _onAcceptHandler.Invoke(args.AcceptSocket);
+            }
+            else
+            {
+                // Accpet 실패
+                Console.WriteLine(args.SocketError.ToString());
+            }
+
+            // 다시 Accept Async
+            RegisterAccept(args);
+        }
+
+        public Socket Accept()
+        {
+            return _listenSocket.Accept();
+        }
+
+    }
+}
